e4a212ea5d3f35ea04fe4d2cbfe461b033771d9e
andrecoghi@gmail.com <andrecoghi@gmail.com>
2019-01-01 22:44:46 -0200

    update code style

--- src/Books.js
+++ src/Books.js
@@ -6,47 +6,47 @@ class Books extends React.Component {
 
     let result = [];
 
-    if (shelf) result = values.filter(book => book.shelf === shelf);
-    else result = values;
+    result = shelf
+      ? values.filter(book => book.shelf === shelf)
+      : (result = values);
 
     return (
       <ol className="books-grid">
         {result &&
-          result
-            .map(book => (
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div
-                      className="book-cover"
-                      style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url(${book.bookCover})`
-                      }}
-                    />
-                    <div className="book-shelf-changer">
-                      <select
-                        value={book.shelf}
-                        onChange={e => onChangeShelf(e, book)}
-                      >
-                        <option value="move" disabled>
-                          Move to...
-                        </option>
-                        <option value="currentlyReading">
-                          Currently Reading
-                        </option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
+          result.map(book => (
+            <li key={book.id}>
+              <div className="book">
+                <div className="book-top">
+                  <div
+                    className="book-cover"
+                    style={{
+                      width: 128,
+                      height: 193,
+                      backgroundImage: `url(${book.bookCover})`
+                    }}
+                  />
+                  <div className="book-shelf-changer">
+                    <select
+                      value={book.shelf}
+                      onChange={e => onChangeShelf(e, book)}
+                    >
+                      <option value="move" disabled>
+                        Move to...
+                      </option>
+                      <option value="currentlyReading">
+                        Currently Reading
+                      </option>
+                      <option value="wantToRead">Want to Read</option>
+                      <option value="read">Read</option>
+                      <option value="none">None</option>
+                    </select>
                   </div>
-                  <div className="book-title">{title}</div>
-                  <div className="book-authors">{book.authors}</div>
                 </div>
-              </li>
-            ))}
+                <div className="book-title">{title}</div>
+                <div className="book-authors">{book.authors}</div>
+              </div>
+            </li>
+          ))}
       </ol>
     );
   }
